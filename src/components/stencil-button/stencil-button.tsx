57f464aafbcec157f0ed3bb80bc455f7caa62489
Eugenia Alcaraz <ealcaraz@altimetrik.com>
2022-03-29 16:16:15 -0300

    Created a button component & working on modal

---
+++ src/components/stencil-button/stencil-button.tsx
@@ -0,0 +1,19 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'stencil-button',
+  styleUrl: 'stencil-button.css',
+  shadow: true,
+})
+export class StencilButton {
+  @Prop() text: string;
+  @Prop() appearance: string;
+
+  render() {
+    return (
+      <button class={`btn ${this.appearance}`} type="button">
+        {this.text}
+      </button>
+    );
+  }
+}
